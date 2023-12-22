656673a4996de2655957ac6f94e022797cacf7c9
tahamazari <tahamazari@gmail.com>
2023-12-22 14:27:21 +0500

    cert id

--- api/models/employee.go
+++ api/models/employee.go
@@ -1,7 +1,9 @@
 package models
 
 type Employee struct {
-	ID    int    `json:"id"`
-	Name  string `json:"name"`
-	Email string `json:"email"`
+	ID            int    `json:"id"`
+	Name          string `json:"name"`
+	Email         string `json:"email"`
+	ContactNumber string `json:"contact_number"`
+	CertificateID string `json:"certificate_id"`
 }
